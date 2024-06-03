3dfdd2bc10526f971d58a13bff4bb75c4ebf621d
Sherilyn <sherilyn_ng@dlsu.edu.ph>
2024-06-03 12:19:41 +0800

    added login to routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -2,8 +2,13 @@ const express = require(`express`);
 const app = express();
 
 
-app.get(`/`, function(req, res) {
+app.get([`/`, `/register`], function(req, res) {
     if (req.session.email == undefined)
         res.render('sign_up', {layout: '/layouts/prelogin.hbs',  title: 'Sign-Up - Filmworks'})});
+
+app.get(`/login`, function(req, res) {
+    if (req.session.email == undefined)
+        res.render('sign_in',  {layout: '/layouts/prelogin.hbs',  title: 'Sign-In - Filmworks'})});
+
 
 module.exports = app;
