5d3508955d9ebd41c72e56a29008433fa77086c8
Sherilyn <sherilyn_ng@dlsu.edu.ph>
2024-06-08 22:28:12 +0800

    adjusted main page to be sign in

--- routes/routes.js
+++ routes/routes.js
@@ -6,12 +6,12 @@ const credentials_controller = require('../controller/credentials_controller')
 
 app.get(`/`, function(req, res) {
     if (req.session.email == undefined)
-        res.render('sign_up', {layout: '/layouts/prelogin.hbs',  title: 'Sign-Up - Filmworks'})
+    res.render('sign_in',  {layout: '/layouts/prelogin.hbs',  title: 'Sign-In - Filmworks'})
 });
 
 app.post(`/register`, function(req, res){
     credentials_controller.successfulRegister
-    res.render('sign_in',  {layout: '/layouts/prelogin.hbs',  title: 'Sign-In - Filmworks'})
+    res.render('sign_up', {layout: '/layouts/prelogin.hbs',  title: 'Sign-Up - Filmworks'})
 })
 
 app.get(`/login`, function(req, res) {
