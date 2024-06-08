6ef18d3785efcd471a565e3444ed470515db2ce7
GelsonSze <94602747+GelsonSze@users.noreply.github.com>
2024-06-09 01:37:41 +0800

    Added rate limiter for login

--- routes/routes.js
+++ routes/routes.js
@@ -17,6 +17,65 @@ const upload = multer({storage: storage})
 
 const credentials_controller = require('../controller/credentials_controller')
 
+// import rate limiter
+const rateLimit = require('express-rate-limit');
+const requestIp = require('request-ip');
+
+// temporary list, will move to db? when implemented
+const bannedIPs = {};
+const checkBan = (req, res, next) => {
+    const ip = req.clientIp;
+    const currentTime = Date.now();
+    
+    // If IP ban has expired, remove it from the banned list
+    if (bannedIPs[ip] && bannedIPs[ip] <= currentTime) {
+        delete bannedIPs[ip];
+    }
+    
+    if (bannedIPs[ip] && bannedIPs[ip] > currentTime) {
+        return res.status(403).json({ message: 'Your IP has been banned.' });
+    }
+    
+    next();
+};
+
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 3, // limit each IP to 3 requests per windowMs
+    message: 'rate limit reached',
+    skip: (req,res ) => res.statusCode == 200,
+    handler:  (req, res, next, options) => {
+        const ip = req.ip;
+        const banDuration = 60 * 1000; // 1 min ban for testing
+        const banExpiry = Date.now() + banDuration;
+
+        // Add IP to the banned list
+        bannedIPs[ip] = banExpiry;
+        res.status(400).json({
+            message: 'You have been banned',
+        });
+    },
+    keyGenerator: (req, res) => {
+        return req.clientIp // IP address 
+    }
+})
+
+const checkValidInput = (req, res, next) => {
+    console.log("TESTING");
+    console.log(req.body)
+    if(req.body.l_email == '123@gmail.com' && req.body.l_password == "123"){
+        res.status(200);
+        next();
+    }
+    else{
+        res.status(400);
+        next();
+    }
+}
+
+app.use(requestIp.mw());
+app.use(checkBan);
+
 
 app.get(`/`, function(req, res) {
     if (req.session.email == undefined)
@@ -28,11 +87,26 @@ app.get(`/register`, function(req, res) {
         res.render('sign_up', {layout: '/layouts/prelogin.hbs',  title: 'Sign-Up - Filmworks'})
 });
 
-app.post(`/postregister`, upload.single('file'), credentials_controller.successfulRegister)
+app.post(`/register`, credentials_controller.successfulRegister, function(req, res){
+    res.render('sign_in',  {layout: '/layouts/prelogin.hbs',  title: 'Sign-In - Filmworks'})
+})
 
 app.get(`/login`, function(req, res) {
     if (req.session.email == undefined)
         res.render('sign_in',  {layout: '/layouts/prelogin.hbs',  title: 'Sign-In - Filmworks'})
 });
+
+app.post(`/login`, checkValidInput, limiter, function(req, res) {
+    console.log(req.rateLimit);
+    if(res.statusCode == 200)
+        res.render('index', {layout: '/layouts/layout.hbs', movie:{}, title: "Main - Filmworks"}) // move to controller next time when db is functioning
+    else{
+        res.render('sign_in',{layout: '/layouts/prelogin.hbs', 
+            error: "",
+            title: 'Sign-In - Filmworks'
+        });
+    }
+});
+
 
 module.exports = app;
