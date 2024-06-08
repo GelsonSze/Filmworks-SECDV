46b32d32f3abf3013b49a1b6ed0f2cefa2a3c552
Sherilyn <sherilyn_ng@dlsu.edu.ph>
2024-06-08 22:31:28 +0800

    added get for register

--- routes/routes.js
+++ routes/routes.js
@@ -8,16 +8,23 @@ app.get(`/`, function(req, res) {
     if (req.session.email == undefined)
     res.render('sign_in',  {layout: '/layouts/prelogin.hbs',  title: 'Sign-In - Filmworks'})
 });
+app.get(`/register`, function(req, res) {
+    if (req.session.email == undefined)
+        res.render('sign_up', {layout: '/layouts/prelogin.hbs',  title: 'Sign-Up - Filmworks'})
+});
+
+
+app.get(`/login`, function(req, res) {
+    if (req.session.email == undefined)
+        res.render('sign_in',  {layout: '/layouts/prelogin.hbs',  title: 'Sign-In - Filmworks'})
+});
+
 
 app.post(`/register`, function(req, res){
     credentials_controller.successfulRegister
     res.render('sign_up', {layout: '/layouts/prelogin.hbs',  title: 'Sign-Up - Filmworks'})
 })
 
-app.get(`/login`, function(req, res) {
-    if (req.session.email == undefined)
-        res.render('sign_in',  {layout: '/layouts/prelogin.hbs',  title: 'Sign-In - Filmworks'})
-});
 
 
 module.exports = app;
