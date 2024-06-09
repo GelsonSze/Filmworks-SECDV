4abef709bbce0c847dc349167421a60c363053ae
GelsonSze <94602747+GelsonSze@users.noreply.github.com>
2024-06-09 14:47:31 +0800

    added captcha to login page

--- routes/routes.js
+++ routes/routes.js
@@ -21,37 +21,30 @@ const rateLimit = require('express-rate-limit');
 const requestIp = require('request-ip');
 
 // temporary list, will move to db? when implemented
-const bannedIPs = {};
+const bannedIPs = [];
 const checkBan = (req, res, next) => {
     const ip = req.clientIp;
-    const currentTime = Date.now();
     
     // If IP ban has expired, remove it from the banned list
-    if (bannedIPs[ip] && bannedIPs[ip] <= currentTime) {
-        delete bannedIPs[ip];
-    }
-    
-    if (bannedIPs[ip] && bannedIPs[ip] > currentTime) {
-        return res.status(403).json({ message: 'Your IP has been banned.' });
+    if (bannedIPs.length != 0 && bannedIPs.includes(ip)) {
+        return res.status(403).json({ message: 'Your IP has been permanently banned.' });
     }
     
     next();
 };
 
 const limiter = rateLimit({
+    requestWasSuccessful: (req, res) => res.statusCode == 200,
+    skipSuccessfulRequests: true,
     windowMs: 5 * 60 * 1000, // 5 minutes
     max: 3, // limit each IP to 3 requests per windowMs
-    message: 'rate limit reached',
-    skip: (req,res ) => res.statusCode == 200,
     handler:  (req, res, next, options) => {
-        const ip = req.ip;
-        const banDuration = 60 * 1000; // 1 min ban for testing
-        const banExpiry = Date.now() + banDuration;
+        const ip = req.clientIp;
 
         // Add IP to the banned list
-        bannedIPs[ip] = banExpiry;
+        bannedIPs.push(ip);
         res.status(400).json({
-            message: 'You have been banned',
+            message: 'Your IP has been permanently banned',
         });
     },
     keyGenerator: (req, res) => {
@@ -62,19 +55,28 @@ const limiter = rateLimit({
 const checkValidInput = (req, res, next) => {
     console.log("TESTING");
     console.log(req.body)
-    if(req.body.l_email == '123@gmail.com' && req.body.l_password == "123"){
-        res.status(200);
-        next();
-    }
-    else{
-        res.status(400);
-        next();
+    console.log(req.recaptcha)
+    if (!req.recaptcha.error) {
+        if(req.body.l_email == '123@gmail.com' && req.body.l_password == "123"){
+            res.status(200);
+            next();
+        }
+        else{
+            res.status(400);
+            next();
+        }
+    } else {
+        res.status(403).send("Forbidden"); // change to whatever we need to do when captcha is failed
     }
 }
 
+//import captcha
+const Recaptcha = require('express-recaptcha').RecaptchaV3
+var options = { hl: 'de' }
+var recaptcha = new Recaptcha('6LcWdvQpAAAAAGmO7xTH5juQyGA99Ye46XycpBif', '6LcWdvQpAAAAACw4JmltpAPbZ_xPJlQag-JNfpDY', options)
+
 app.use(requestIp.mw());
 app.use(checkBan);
-
 
 app.get(`/`, function(req, res) {
     if (req.session.email == undefined)
@@ -93,8 +95,8 @@ app.get(`/login`, function(req, res) {
         res.render('sign_in',  {layout: '/layouts/prelogin.hbs',  title: 'Sign-In - Filmworks'})
 });
 
-app.post(`/login`, checkValidInput, limiter, function(req, res) {
-    console.log(req.rateLimit);
+app.post(`/login`, recaptcha.middleware.verify, checkValidInput, limiter, function(req, res) {
+    console.log(req.rateLimit)
     if(res.statusCode == 200)
         res.render('index', {layout: '/layouts/layout.hbs', movie:{}, title: "Main - Filmworks"}) // move to controller next time when db is functioning
     else{
