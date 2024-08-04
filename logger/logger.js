182ab0ff1ee78f9566fd286d460bfd55a4f8027c
Rafu <ralph_furigay@dlsu.edu.ph>
2024-08-04 20:51:41 +0800

    fixed logs, finally working

--- logger/logger.js
+++ logger/logger.js
@@ -1,50 +1,57 @@
 const winston = require('winston');
 const {combine, timestamp, printf} = winston.format
+const path = require('path')
 
 winston.loggers.add('UserActivityLogger', {
+    level: 'info',
     format: combine(
         timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
         printf((info) => `[${info.timestamp}] ${info.message}` )
     ),
-    transports: new winston.transports.File({filename: '../logs/user_activity.log'})
+    transports: [new winston.transports.File({filename: path.resolve(__dirname, "../logs/user_activity.log")})]
 })
 
 winston.loggers.add('RegistrationLogger', {
+    level: 'info',
     format: combine(
         timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
         printf((info) => `[${info.timestamp}] ${info.message}` )
     ),
-    transports: new winston.transports.File({filename: '../logs/registration.log'})
+    transports: [new winston.transports.File({filename: path.resolve(__dirname, "../logs/registration.log")})]
 })
 
 winston.loggers.add('AuthenticationLogger', {
+    level: 'info',
     format: combine(
         timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
         printf((info) => `[${info.timestamp}] ${info.message}` )
     ),
-    transports: new winston.transports.File({filename: '../logs/authentication.log'})
+    transports: [new winston.transports.File({filename: path.resolve(__dirname, "../logs/authentication.log")})]
 })
 
 winston.loggers.add('TransactionLogger', {
+    level: 'info',
     format: combine(
         timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
         printf((info) => `[${info.timestamp}] ${info.message}` )
     ),
-    transports: new winston.transports.File({filename: '../logs/transaction.log'})
+    transports: [new winston.transports.File({filename: path.resolve(__dirname, "../logs/transaction.log")})]
 })
 
 winston.loggers.add('AdminLogger', {
+    level: 'info',
     format: combine(
         timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
         printf((info) => `[${info.timestamp}] ${info.message}` )
     ),
-    transports: new winston.transports.File({filename: '../logs/admin.log'})
+    transports: [new winston.transports.File({filename: path.resolve(__dirname, "../logs/admin.log")})]
 })
 
 winston.loggers.add('DevLogger', {
+    level: 'info',
     format: combine(
         timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
         printf((info) => `[${info.timestamp}] ${info.message}` )
     ),
-    transports: new winston.transports.Console()
+    transports: [new winston.transports.Console()]
 })
